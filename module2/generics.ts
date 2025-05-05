7fe7ea6c6d76ca5ca7e3a3d1c42911afe9b0110d
sohun-cs <asaduzzamansohun5@gmail.com>
2025-05-06 00:56:29 +0600

    Generic Type

---
+++ module2/generics.ts
@@ -0,0 +1,45 @@
+{
+
+    // Generic Type
+
+    type GenericArray<T> = Array<T>;
+
+    // const rollNumbers: number[] = [2, 3, 5 ,3];
+    // const rollNumbers: Array<number> = [2, 3, 5 ,3];
+    const rollNumbers: GenericArray<number> = [2, 3, 5, 3];
+
+
+    // const mentors: string[] = ["Mr. X", "Mr. Y", "Mr. Z"];
+    // const mentors: Array<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
+    const mentors: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
+
+    // const boolArray: boolean[] = [true, false, true];
+    // const boolArray: Array<boolean> = [true, false, true];
+    const boolArray: GenericArray<boolean> = [true, false, true];
+
+
+    const user: GenericArray<{ name: string; age: number }> = [
+        {
+            name: "Mezba",
+            age: 100
+        },
+        {
+            name: "Jhankar Mahbub",
+            age: 110
+        }
+    ]
+
+
+    // Generic Tuple
+
+    type GenericTuple<X, Y> = [X, Y];
+
+    // const manush: [string, string] = ['Mr. X', 'Mr. Y'];
+    const manush: GenericTuple<string, string> = ['Mr. X', 'Mr. Y'];
+
+    const UserWithID: GenericTuple<number, { name: string, email: string }> = [1234, { name: 'Persian', email: 'a@gmail.com' }];
+
+
+
+
+}
