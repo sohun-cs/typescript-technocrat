2817e76891111b7d5a8bfda91749014e69a8e008
sohun-cs <asaduzzamansohun5@gmail.com>
2025-05-16 21:54:45 +0600

    encapsulation

--- module3/encapsulation.ts
+++ module3/encapsulation.ts
@@ -1,3 +1,46 @@
 {
-    
+
+    // Encapsulation
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        private _balance: number;
+
+
+        constructor(id: number, name: string, _balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = _balance;
+        }
+
+        addDeposit(amount: number) {
+            this._balance = this._balance + amount;
+        }
+
+        // 1
+        private getBalance() {
+            return this._balance;
+        }
+
+
+        getHiddenMethod() {
+            return this.getBalance();
+        }
+
+    };
+
+
+    class StudentAccount extends BankAccount {
+        test() {
+            this.getHiddenMethod()
+        }
+    }
+
+
+    const myAccount = new BankAccount(111, "Sohun", 20);
+
+    myAccount.addDeposit(200);
+    const myBalance = myAccount.getHiddenMethod();
+    console.log(myBalance);
+
 }
